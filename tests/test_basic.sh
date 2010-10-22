396db3b0b3662374b878d2f8ac7744afaddb96a0
enol@kiru.local <enol@kiru.local>
2010-10-22 10:46:50 +0200

    Added support for testing in BSD style systems.

--- tests/test_basic.sh
+++ tests/test_basic.sh
@@ -4,6 +4,19 @@
 # Tests for MPI-Start with dummy environment
 #
 
+# check mktemp
+TMPFILE=`mktemp 2> /dev/null`
+if test $? -ne 0 ; then
+    alias mktemp='mktemp -t MPI_START_TESTS'
+    TMPFILE=`mktemp 2> /dev/null`
+    if test $? -ne 0 ; then
+        echo "Unable to find good mktemp!?"
+        exit 0
+    fi
+fi
+rm -f $TMPFILE    
+
+
 setUp () {
     export I2G_MPI_START=../bin/mpi-start
     export I2G_MPI_TYPE="dummy"
