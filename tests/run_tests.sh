938dd0b003cdd605d2a7964c3cd8ebfaf4cb7ebf
Enol Fernandez <enolfc@ifca.unican.es>
2011-01-31 16:58:42 +0100

    Changed messages.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -7,16 +7,16 @@ DOWNLOAD_MY_SHUNIT=1
 # Check environment variables
 #
 if test "x${SHUNIT2}" = "x" ; then
-    echo "SHUNIT2 environment variable not defined!"
     if test "x${DOWNLOAD_MY_SHUNIT}" = "x1"; then
         wget -q http://devel.ifca.es/~enol/depot/shunit2 -O shunit2 
         st=$?
         if test $st -ne 0 ; then
-            echo "Could not download shunit..."
+            echo "Could not download shunit, please set SHUNIT2 env variable to the correct location."
             exit 1
         fi
         export SHUNIT2=$PWD/shunit2
     else
+        echo "SHUNIT2 environment variable not defined!"
         echo "Please set it to the location of shunit2 script"
         exit 1
     fi
