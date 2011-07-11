e5295beed791dd7f671394c3dca651dbc9225040
Enol Fernandez <enolfc@ifca.unican.es>
2011-07-11 12:32:49 +0200

    Added check for environment variable definition.

--- tests/test_mpi.sh
+++ tests/test_mpi.sh
@@ -24,11 +24,23 @@ int main (int argc, char *argv[]) {
     MPI_Init(&argc,&argv);
     MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD,&myid);
-
     if (myid == 0) {
         printf("NP = %d", numprocs);
     }
 
+    if (argc > 1) {
+        /* Environ variable checks */
+        int i;
+        for (i = 1; i < argc; i += 2) {
+            char *env = getenv(argv[i]);
+            if (!env) {
+                fprintf(stderr, "%s not defined!\n", argv[i]);
+            } else {
+                if (i + 1 >= argc) continue;
+                if (strcmp(env, argv[i + 1])) fprintf(stderr, "%s value is not %s", env, argv[i+1]);
+            }
+        }
+    }
     MPI_Finalize();
     return 0;
 }
