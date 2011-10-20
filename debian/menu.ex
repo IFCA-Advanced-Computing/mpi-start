1a11d391cf5fab8709b7941dbf50ae1c67164b79
Enol Fernandez <enolfc@ifca.unican.es>
2011-10-20 12:04:53 +0200

    New debian dir for building packages.

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(mpi-start):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="mpi-start" command="/usr/bin/mpi-start"
---
+++ debian/mpi-start.default.ex
@@ -0,0 +1,10 @@
+# Defaults for mpi-start initscript
+# sourced by /etc/init.d/mpi-start
+# installed at /etc/default/mpi-start by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
