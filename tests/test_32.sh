e2a4696ce857cf473c41e1087b505ac136d61312
Enol Fernandez <enolfc@ifca.unican.es>
2011-11-02 17:44:20 +0100

    New test for bug #32.

---
+++ tests/test_32.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# tests for bug #32
+
+setUp () {
+    export MYPWD=$PWD
+    export MYDIR=`$MYMKTEMP -d`
+    export I2G_MPI_TYPE="dummy"
+    unset I2G_MPI_NP
+    unset I2G_MPI_APPLICATION
+    unset I2G_MPI_START_DEBUG
+    unset I2G_MPI_START_VERBOSE
+    unset I2G_MPI_START_TRACE
+    unset I2G_MPI_SINGLE_PROCESS
+    unset I2G_MPI_NP
+    unset I2G_MPI_PER_NODE
+    export MPI_START_SHARED_FS=0
+    export MPI_START_DUMMY_SCHEDULER=0
+    export PBS_NODEFILE=`$MYMKTEMP`
+    cat > $PBS_NODEFILE << EOF
+host1
+host1
+host2
+host2
+host3
+host3
+host3
+host3
+EOF
+}
+
+tearDown () {
+    rm -f $PBS_NODEFILE
+    unset PBS_NODEFILE
+    cd $MYPWD
+    rm -rf $MYDIR
+}
+
+testCopyAndCleanCall() {
+    cd $MYDIR
+    export I2G_MPI_FILE_DIST=cptoshared
+    export MPI_SHARED_HOME_PATH=$MYDIR
+    mkdir -p one/two
+    touch one/two/1
+    export I2G_MPI_APPLICATION=`$MYMKTEMP`
+    cat > $I2G_MPI_APPLICATION << EOF
+#!/bin/sh
+echo "\${SHARED_BASE_PATH}"
+/bin/ls \${SHARED_BASE_PATH} > /dev/null
+EOF
+    SHARED_DIR=`$I2G_MPI_START`
+    assertEquals "0" "$?"
+    # the directory should not exist
+    ls $SHARED_DIR 2> /dev/null
+    assertNotEquals "0" "$?"
+}
+
+. $SHUNIT2
