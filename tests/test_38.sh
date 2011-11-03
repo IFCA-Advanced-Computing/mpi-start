3cabd622a5f24ea8ab4738b3eabf63ccb8cde916
Enol Fernandez <enolfc@ifca.unican.es>
2011-11-03 17:07:33 +0100

    Adapted test to new loading mechanisms of mpirun/mpiexec.

--- tests/test_38.sh
+++ tests/test_38.sh
@@ -7,22 +7,22 @@
 export I2G_MPI_START_ENABLE_TESTING="TEST"
 # source the mpi-start code to have all functions
 . $I2G_MPI_START
-check_options
+mpi_start_check_options
+
+export MPI_OPENMPI_MPIEXEC=`$MYMKTEMP`
+cat > $MPI_OPENMPI_MPIEXEC << EOF
+#/bin/sh
+exit 0
+EOF
+chmod +x $MPI_OPENMPI_MPIEXEC
 
 MPI_START_SCHEDULER="pbs"
 MPI_START_DISABLE_LRMS_INTEGRATION="no"
 
 
-# load openmpi
-. $MPI_START_ETC/openmpi.mpi
-
 testBug38() {
-    export MPI_OPENMPI_MPIEXEC=`$MYMKTEMP`
-    cat > $MPI_OPENMPI_MPIEXEC << EOF
-#/bin/sh
-exit 0
-EOF
-    chmod +x $MPI_OPENMPI_MPIEXEC
+    # load openmpi
+    . $MPI_START_ETC/openmpi.mpi
     mpi_exec
     $MPI_OPENMPI_INFO --parseable | grep "pls:tm" &> /dev/null
     if test $? -eq 0 ; then
