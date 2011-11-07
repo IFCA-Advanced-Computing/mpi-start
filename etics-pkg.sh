9d80878475b8e8b878af97436430fc572776301f
Enol Fernandez <enolfc@ifca.unican.es>
2011-11-07 11:53:48 +0100

    Debian package building for ETICS.

--- etics-pkg.sh
+++ etics-pkg.sh
@@ -4,44 +4,75 @@ set -x
 
 prefix=$1
 
-if [ "x$prefix" = "x" ] ; then
-    echo "Unable to build without a prefix"
-    exit 1
+if [ "x$prefix" != "x" ] ; then
+    shift
+    version=$1
 fi
-
-shift
-
-version=$1
 
 if [ "x$version" = "x" ] ; then
     version=`cat VERSION`
 fi
-
 BASEDIR=$PWD
 
-# delete old artifacts
-rm -rf $BASEDIR/tgz $BASEDIR/RPMS
+debian_package() {
+    # dest dir: debs
+    DESTDIR=debs
+    mkdir $BASEDIR/$DESTDIR
+    make deb
+    mv ../mpi-start_*.dsc $DESTDIR
+    mv ../mpi-start_*.tar.gz $DESTDIR
+    mv ../mpi-start_*.deb $DESTDIR
+    mv ../mpi-start_*.changes $DESTDIR
+}
 
-# create destination directories
-mkdir $BASEDIR/tgz
-mkdir $BASEDIR/RPMS 
+rpm_package() {
+    if [ "x$prefix" = "x" ] ; then
+        echo "Unable to build without a prefix"
+        exit 1
+    fi
 
-# Binary tarball
-cd $prefix
-tar -pczf $BASEDIR/tgz/mpi-start-$version.tar.gz *
-cd $BASEDIR
+    # delete old artifacts
+    rm -rf $BASEDIR/tgz $BASEDIR/RPMS
 
-# Source tarball
-make dist
-distname=`ls *tar.gz | tail -1`
-srcname=`echo $distname | sed 's/.tar.gz/.src.tar.gz/'`
-mv $distname $BASEDIR/tgz/$srcname
+    # create destination directories
+    mkdir $BASEDIR/tgz
+    mkdir $BASEDIR/RPMS 
 
-# RPMs
-make rpm
-mv rpms/RPMS/noarch/*.rpm $BASEDIR/RPMS
-mv rpms/SRPMS/*.rpm $BASEDIR/RPMS
+    # Binary tarball
+    cd $prefix
+    tar -pczf $BASEDIR/tgz/mpi-start-$version.tar.gz *
+    cd $BASEDIR
 
-# Debian?
+    # Source tarball
+    make dist
+    distname=`ls *tar.gz | tail -1`
+    srcname=`echo $distname | sed 's/.tar.gz/.src.tar.gz/'`
+    mv $distname $BASEDIR/tgz/$srcname
+
+    # RPMs
+    make rpm
+    mv rpms/RPMS/noarch/*.rpm $BASEDIR/RPMS
+    mv rpms/SRPMS/*.rpm $BASEDIR/RPMS
+}
+
+DEBIAN=0
+which lsb_release > /dev/null
+if [ $? -eq 0 ] ; then
+    lsb_release -a | egrep -i "debian|ubuntu" > /dev/null
+    if [ $? -eq 0 ] ; then
+        DEBIAN=1
+    fi
+else
+    cat /etc/issue | grep -i "debian|ubuntu" > /dev/null 
+    if [ $? -eq 0 ] ; then
+        DEBIAN=1
+    fi
+fi
+
+if [ $DEBIAN -eq 1 ] ; then
+    debian_package
+else
+    rpm_package
+fi
 
 exit 0
