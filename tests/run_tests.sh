87ab056a53fed5a05e811243b9fd8b56e0f9a7d5
Enol Fernandez <enolfc@ifca.unican.es>
2011-12-16 10:01:39 +0100

    Starting preparation of Version 1.3

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -65,7 +65,7 @@ if test "x${SHUNIT2}" = "x" ; then
 fi
 
 if test "x${I2G_MPI_START}" = "x" ; then
-    type mpi-start &> /dev/null
+    which mpi-start &> /dev/null
     if test $? -ne 0 ; then
         echo "I2G_MPI_START environment variable not defined!"
         echo "Please set it to the location of MPI-Start binary"
