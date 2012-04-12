eb74946c7e2865d3772c5be319cef9b46e544173
Enol Fernandez <enolfc@ifca.unican.es>
2012-04-12 17:13:05 +0200

    New debian 6 deployment tests!

---
+++ tests/deploy/deb6-emi2-wn.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# testing script for sl6 + emi2 installation
+
+bash ./install-wn-emi2-deb6.sh
+bash ./config-wn-deb6.sh
---
+++ tests/deploy/install-wn-emi2-deb6.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+################
+# INSTALLATION #
+################
+
+echo "*"
+echo "* Installation "
+echo "*"
+
+echo "** Get Repos:"
+
+echo "***          CAs"
+wget -q -O - \
+     https://dist.eugridpma.info/distribution/igtf/current/GPG-KEY-EUGridPMA-RPM-3 \
+     | apt-key add -
+echo "deb http://repository.egi.eu/sw/production/cas/1/current egi-igtf core" > /etc/apt/sources.list.d/cas.list
+
+
+echo "***          emi 2 (rc4)"
+wget --no-check-certificate https://twiki.cern.ch/twiki/pub/EMI/EMI-2/emi-2-rc4-deb6.list -O /etc/apt/sources.list.d/emi2.list
+apt-get update
+
+
+echo "** apt-get Update"
+apt-get update
+
+# install mpi packages
+echo "** Install MPI Packages"
+apt-get -y install openmpi-devel openmpi-bin mpich2 mpich-bin libmpich2-dev  libmpich1.0-dev lam-dev  lam-runtime
+
+echo "** Install CAs"
+apt-get -y install ca-policy-egi-core
+
+# install emi-mpi
+echo "** emi-mpi"
+apt-get -y install emi-mpi
+if [ $? -ne 0 ] ; then
+    echo "******************************************************"
+    echo "ERROR installing emi-mpi!"
+    echo "******************************************************"
+    exit 1
+fi
+
+# emi-wn is not available, install glite-yaim-clients instead
+# and other dependencies: gawk, fetch-crl
+echo "** emi-wn"
+apt-get -y install glite-yaim-clients gawk fetch-crl
+
+echo "******************************************************"
+echo " INSTALLATION SUCCEDED!"
+echo "******************************************************"
+exit 0
