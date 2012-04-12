93afe8cfcbaf9b3e45f0969348e1f17553993914
Enol Fernandez <enolfc@ifca.unican.es>
2012-04-12 12:42:03 +0200

    Fix sl6 configuration for WN.

--- tests/deploy/config-wn-sl6.sh
+++ tests/deploy/config-wn-sl6.sh
@@ -156,8 +156,8 @@ cat /etc/yaim/site-info.def | grep MPI
 cat > /tmp/env << EOF
 I2G_MPI_START=/usr/bin/mpi-start
 MPI_DEFAULT_FLAVOUR=openmpi
-MPI_OPENMPI_PATH=/usr/lib64/openmpi/1.4-gcc
-MPI_OPENMPI_VERSION=1.4
+MPI_OPENMPI_PATH=/usr/lib64/openmpi
+MPI_OPENMPI_VERSION=1.5.3
 MPI_SHARED_HOME=no
 EOF
 diff_configs /tmp/env
@@ -174,8 +174,8 @@ cat /etc/yaim/site-info.def | grep MPI
 cat > /tmp/env << EOF
 I2G_MPI_START=/usr/bin/mpi-start
 MPI_DEFAULT_FLAVOUR=openmpi
-MPI_OPENMPI_PATH=/usr/lib64/openmpi/1.4-gcc
-MPI_OPENMPI_VERSION=1.4
+MPI_OPENMPI_PATH=/usr/lib64/openmpi
+MPI_OPENMPI_VERSION=1.5.3
 MPI_START_SHARED_FS=1
 EOF
 diff_configs /tmp/env
@@ -193,19 +193,18 @@ cat /etc/yaim/site-info.def | grep MPI
 cat > /tmp/env << EOF
 I2G_MPI_START=/usr/bin/mpi-start
 MPI_DEFAULT_FLAVOUR=openmpi
-MPI_OPENMPI_PATH=/usr/lib64/openmpi/1.4-gcc
-MPI_OPENMPI_VERSION=1.4
+MPI_OPENMPI_PATH=/usr/lib64/openmpi
+MPI_OPENMPI_VERSION=1.5.3
 MPI_SHARED_HOME_PATH=/tmp
 MPI_SHARED_HOME=yes
 EOF
 diff_configs /tmp/env
 
-# openmpi + mpich2 + lam 
+# openmpi + mpich2
 cat /etc/yaim/site-info.def.orig > /etc/yaim/site-info.def
 cat >> /etc/yaim/site-info.def << EOF
 #### MPI CONFIGURATION
 MPI_OPENMPI_ENABLE="yes"
-MPI_LAM_ENABLE="yes"
 MPI_MPICH2_ENABLE="yes"
 EOF
 configure_ok
@@ -213,12 +212,10 @@ cat /etc/yaim/site-info.def | grep MPI
 cat > /tmp/env << EOF
 I2G_MPI_START=/usr/bin/mpi-start
 MPI_DEFAULT_FLAVOUR=openmpi
-MPI_LAM_PATH=/usr/lib64/lam
-MPI_LAM_VERSION=7.1.2
 MPI_MPICH2_PATH=/usr/lib64/mpich2
-MPI_MPICH2_VERSION=1.2.1p1
-MPI_OPENMPI_PATH=/usr/lib64/openmpi/1.4-gcc
-MPI_OPENMPI_VERSION=1.4
+MPI_MPICH2_VERSION=1.2.1
+MPI_OPENMPI_PATH=/usr/lib64/openmpi
+MPI_OPENMPI_VERSION=1.5.3
 EOF
 diff_configs /tmp/env
 
@@ -231,8 +228,7 @@ mv /tmp/mpi-start-* /tmp/mpi-start
 cat /tmp/mpi-start/tests/run_tests.sh | \
     sed 's/RUN_OMP_TESTS=0/RUN_OMP_TESTS=1/' | \
     sed 's/RUN_MPICH2_TESTS=0/RUN_MPICH2_TESTS=1/' |\
-    sed 's/RUN_OPENMPI_TESTS=0/RUN_OPENMPI_TESTS=1/' | \
-    sed 's/RUN_LAM_TESTS=0/RUN_LAM_TESTS=1/' > /tmp/mpi-start/tests/runner.sh
+    sed 's/RUN_OPENMPI_TESTS=0/RUN_OPENMPI_TESTS=1/' > /tmp/mpi-start/tests/runner.sh
 chmod +x /tmp/mpi-start/tests/runner.sh
 chown -R dteam001:dteam /tmp/mpi-start/
 
