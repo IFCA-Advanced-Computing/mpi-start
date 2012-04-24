ffc89e1314ce14d51f353105849fcad1d7ed1b9c
Enol Fernandez <enolfc@ifca.unican.es>
2012-04-24 19:06:00 +0200

    Just installation.

--- tests/deploy/install-ce-emi2-sl5.sh
+++ tests/deploy/install-ce-emi2-sl5.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# installation script for WN on sl5 + emi2 
 
 ################
 # INSTALLATION #
@@ -9,33 +8,7 @@ echo "*"
 echo "* Installation "
 echo "*"
 
-echo "** Get Repos:"
-echo "***          Epel"
-## epel
-wget http://download.fedoraproject.org/pub/epel/5/i386/epel-release-5-4.noarch.rpm
-yum -q -y localinstall epel-release-5-4.noarch.rpm
-if [ $? -ne 0 ] ; then exit 1; fi
-
-#echo "***          Trust Anchors"
-## Trust Anchors
-wget http://repository.egi.eu/sw/production/cas/1/current/repo-files/egi-trustanchors.repo -O /etc/yum.repos.d/egi-trust.repo
-
-#echo "***          emi 2 (rc4)"
-# enable repos: emi 2
-wget --no-check-certificate  https://twiki.cern.ch/twiki/pub/EMI/EMI-2/emi-2-rc4-sl5.repo -O /etc/yum.repos.d/emi2.repo
-if [ $? -ne 0 ] ; then exit 1; fi
-
-## update 
-echo "** YUM Update"
-yum -q -y update
- install lcg-ca 
-echo "** Install CAs"
-yum -q -y install ca-policy-egi-core
-if [ $? -ne 0 ] ; then exit 1; fi
-
-
 echo "** EMI-CREAM + Torque"
-# Install needed tools for CE
 yum -q -y install emi-cream-ce emi-torque-server emi-torque-utils
 if [ $? -ne 0 ] ; then
     echo "******************************************************"
