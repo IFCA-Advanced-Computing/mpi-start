949f5a10ccaa62a6739b01db473f62fe5485cbc5
Enol Fernandez <enolfc@ifca.unican.es>
2012-04-24 19:39:41 +0200

    Keep on cleaning.

--- tests/deploy/install-wn-emi2-deb6.sh
+++ tests/deploy/install-wn-emi2-deb6.sh
@@ -1,28 +1,8 @@
 #!/bin/sh
 
-################
-# INSTALLATION #
-################
-
 echo "*"
 echo "* Installation "
 echo "*"
-
-echo "** Get Repos:"
-
-echo "***          CAs"
-wget -q -O - \
-     https://dist.eugridpma.info/distribution/igtf/current/GPG-KEY-EUGridPMA-RPM-3 \
-     | apt-key add -
-echo "deb http://repository.egi.eu/sw/production/cas/1/current egi-igtf core" > /etc/apt/sources.list.d/cas.list
-
-
-echo "***          emi 2 (rc4)"
-wget --no-check-certificate https://twiki.cern.ch/twiki/pub/EMI/EMI-2/emi-2-rc4-deb6.list -O /etc/apt/sources.list.d/emi2.list
-
-
-echo "** apt-get Update"
-apt-get -q update
 
 # install mpi packages
 echo "** Install MPI Packages"
