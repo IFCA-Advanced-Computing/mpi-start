b2f6d406ce00244d3ee48b5a8260d2e87b45e414
Enol Fernandez <enolfc@ifca.unican.es>
2012-04-25 11:00:57 +0200

    Use rc4 repo instead of rc final.

--- tests/deploy/repo-sl6.sh
+++ tests/deploy/repo-sl6.sh
@@ -9,16 +9,16 @@ echo "*"
 echo "* Repository Configuration"
 echo "*"
 
-echo "** Epel"
-wget http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-5.noarch.rpm
-yum -q -y localinstall epel-release-6-5.noarch.rpm
-if [ $? -ne 0 ] ; then exit 1; fi
+#echo "** Epel"
+#wget http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-5.noarch.rpm
+#yum -q -y localinstall epel-release-6-5.noarch.rpm
+#if [ $? -ne 0 ] ; then exit 1; fi
 
-echo "** Trust Anchors"
-wget http://repository.egi.eu/sw/production/cas/1/current/repo-files/egi-trustanchors.repo -O /etc/yum.repos.d/egi-trust.repo
+#echo "** Trust Anchors"
+#wget http://repository.egi.eu/sw/production/cas/1/current/repo-files/egi-trustanchors.repo -O /etc/yum.repos.d/egi-trust.repo
 
 echo "** emi 2"
-wget --no-check-certificate  https://twiki.cern.ch/twiki/pub/EMI/EMI-2/emi-2-rc-sl6.repo -O /etc/yum.repos.d/emi2.repo
+wget --no-check-certificate  https://twiki.cern.ch/twiki/pub/EMI/EMI-2/emi-2-rc4-sl6.repo -O /etc/yum.repos.d/emi2.repo
 if [ $? -ne 0 ] ; then exit 1; fi
 
 ## update 
