95b66ed0ec54fc87eb5e1b01ec8b4862ccc9047d
Enol Fernandez <enolfc@ifca.unican.es>
2012-07-31 16:53:23 +0200

    Finalized test for #60.

--- tests/test_60.sh
+++ tests/test_60.sh
@@ -1,64 +1,126 @@
 #!/bin/bash
 
-# tests for bug #32
+# tests for bug #60
 
-setUp () {
-    export MYPWD=$PWD
+oneTimeSetUp() {
     export MYDIR=`$MYMKTEMP -d`
-    export I2G_MPI_TYPE="dummy"
-    unset I2G_MPI_NP
-    unset I2G_MPI_APPLICATION
-    unset I2G_MPI_START_DEBUG
-    unset I2G_MPI_START_VERBOSE
-    unset I2G_MPI_START_TRACE
-    unset I2G_MPI_SINGLE_PROCESS
-    unset I2G_MPI_NP
-    unset I2G_MPI_PER_NODE
-    export MPI_START_SHARED_FS=0
-    export MPI_START_DUMMY_SCHEDULER=0
-    export MPI_START_DISABLE_CLEANUP="no"
-    export PBS_NODEFILE=`$MYMKTEMP`
-    cat > $PBS_NODEFILE << EOF
-host1
-host1
-host2
-host2
-host3
-host3
-host3
-host3
-EOF
+    pushd $MYDIR > /dev/null
+    TARBALL=`$MYMKTEMP`
+    mkdir -p one/two
+    touch one/two/1
+    touch testapp
+    tar czf $TARBALL $MYDIR 2> /dev/null
+
+    export I2G_MPI_START_ENABLE_TESTING="TEST"
+    # source the mpi-start code to have all functions
+    . $I2G_MPI_START
+    mpi_start_check_options
+    mpi_start_get_plugin "cptoshared.filedist" 1
+    . $MPI_START_PLUGIN_FILES 
+    popd > /dev/null
 }
 
-tearDown () {
-    rm -f $PBS_NODEFILE
-    unset PBS_NODEFILE
-    cd $MYPWD
+oneTimeTearDown() {
     rm -rf $MYDIR
 }
 
-testTOBEWRITTEN() {
-    # XXX
-    assertEquals "0" "1"
-#    cd $MYDIR
-#    export I2G_MPI_FILE_DIST=cptoshared
-#    export MPI_SHARED_HOME_PATH=$MYDIR
-#    mkdir -p one/two
-#    touch one/two/1
-#    export I2G_MPI_APPLICATION="mpiapp"
-#    cat > $I2G_MPI_APPLICATION << EOF
-##!/bin/sh
-#echo "\${SHARED_BASE_PATH}"
-#/bin/ls one/two/1 > /dev/null && /bin/ls \${SHARED_BASE_PATH} > /dev/null
-#EOF
-#    SHARED_DIR=`$I2G_MPI_START`
-#    st=$?
-#    assertEquals "0" "$st"
-#    # the directory should not exist
-#    ls $SHARED_DIR 2> /dev/null
-#    st=$?
-#    echo $st
-#    assertNotEquals "0" "$st"
+setUp() {
+    pushd $MYDIR > /dev/null
+    export MPI_SHARED_HOME_PATH=`$MYMKTEMP -d`
+    unset I2G_MPI_APPLICATION
+}
+
+tearDown() {
+    popd > /dev/null
+    rm -rf $MPI_SHARED_HOME_PATH
+}
+
+testRelativeInGlobalPath() {
+    export I2G_MPI_APPLICATION=ls
+    copy
+    st=$?
+    assertEquals "0" "$st"
+    assertEquals "ls" "$I2G_MPI_APPLICATION"
+}
+
+testRelativeInPWD() {
+    export I2G_MPI_APPLICATION=testapp
+    copy
+    st=$?
+    assertEquals "0" "$st"
+    assertEquals "$SHARED_BASE_PATH/$MYDIR/testapp" "$I2G_MPI_APPLICATION"
+    stat $I2G_MPI_APPLICATION > /dev/null
+    st=$?
+    assertEquals "0" "$st"
+}
+
+testRelativeUnderPWD() {
+    export I2G_MPI_APPLICATION=one/two/1
+    chmod -x one/two/1
+    copy
+    st=$?
+    assertEquals "0" "$st"
+    assertEquals "$SHARED_BASE_PATH/$MYDIR/one/two/1" "$I2G_MPI_APPLICATION"
+    stat $I2G_MPI_APPLICATION > /dev/null
+    st=$?
+    assertEquals "0" "$st"
+}
+
+testRelativeInPWDinPATH() {
+    oldPATH="$PATH"
+    PATH=.:$PATH
+    export I2G_MPI_APPLICATION=testapp
+    chmod +x testapp
+    copy
+    st=$?
+    assertEquals "0" "$st"
+    assertEquals "$SHARED_BASE_PATH/$MYDIR/testapp" "$I2G_MPI_APPLICATION"
+    stat $I2G_MPI_APPLICATION > /dev/null
+    st=$?
+    assertEquals "0" "$st"
+    PATH=$oldPATH
+}
+
+testRelativeUnderPWDinPATH() {
+    export I2G_MPI_APPLICATION=one/two/1
+    chmod +x one/two/1
+    copy
+    st=$?
+    assertEquals "0" "$st"
+    assertEquals "$SHARED_BASE_PATH/$MYDIR/one/two/1" "$I2G_MPI_APPLICATION"
+    stat $I2G_MPI_APPLICATION > /dev/null
+    st=$?
+    assertEquals "0" "$st"
+}
+
+testAbsoluteNotInPWD() {
+    export I2G_MPI_APPLICATION=/bin/ls
+    copy
+    st=$?
+    assertEquals "0" "$st"
+    assertEquals "/bin/ls" "$I2G_MPI_APPLICATION"
+}
+
+testAbsoluteInPWD() {
+    export I2G_MPI_APPLICATION=$PWD/testapp
+    copy
+    st=$?
+    assertEquals "0" "$st"
+    assertEquals "$SHARED_BASE_PATH/$MYDIR/testapp" "$I2G_MPI_APPLICATION"
+    stat $I2G_MPI_APPLICATION > /dev/null
+    st=$?
+    assertEquals "0" "$st"
+}
+
+testAbsoluteUnderPWD() {
+    export I2G_MPI_APPLICATION=$PWD/one/two/1
+    copy
+    st=$?
+    assertEquals "0" "$st"
+    assertEquals "$SHARED_BASE_PATH/$MYDIR/one/two/1" "$I2G_MPI_APPLICATION"
+    stat $I2G_MPI_APPLICATION > /dev/null
+    st=$?
+    assertEquals "0" "$st"
 }
 
 . $SHUNIT2
